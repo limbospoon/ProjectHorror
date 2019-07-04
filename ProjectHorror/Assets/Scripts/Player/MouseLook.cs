9ba45262464438a2a6fbea1e8cb61fc62c06993d
John McCormack <silentsammy777@hotmail.com>
2019-07-04 21:35:10 +0100

    Intial Commit

---
+++ ProjectHorror/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [Range(1.0f, 20.0f)]
+    public float xSensitivity = 15.0f;
+    [Range(1.0f, 20.0f)]
+    public float ySensitivity = 15.0f;
+    public enum Mode
+    {
+        XandY,
+        XOnly,
+        YOnly
+    }
+    public Mode mode;
+
+    private float rotX = 0f;
+    private float rotY = 0f;
+
+    private Quaternion ogRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ogRot = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(mode)
+        {
+            case Mode.XandY:
+                {
+
+                    rotX += Input.GetAxis("Mouse X") * xSensitivity;
+                    rotY += Input.GetAxis("Mouse Y") * ySensitivity;
+
+                    rotX = ClampAngle(rotX, -360.0f, 360.0f);
+                    rotY = ClampAngle(rotY, -60, 60);
+
+                    Quaternion xQuat = Quaternion.AngleAxis(rotX, Vector3.up);
+                    Quaternion yQuat = Quaternion.AngleAxis(rotY, -Vector3.right);
+
+                    transform.localRotation = ogRot * xQuat * yQuat;
+                    break;
+                }
+            case Mode.XOnly:
+                {
+                    rotX += Input.GetAxis("Mouse X") * xSensitivity;
+                    
+                    rotX = ClampAngle(rotX, -360.0f, 360.0f);
+                    
+                    Quaternion xQuat = Quaternion.AngleAxis(rotX, Vector3.up);
+                    
+                    transform.localRotation = ogRot * xQuat;
+                    break;
+                }
+            case Mode.YOnly:
+                {
+                    rotY += Input.GetAxis("Mouse Y") * ySensitivity;
+                    rotY = ClampAngle(rotY, -60, 60);
+
+                    Quaternion yQuat = Quaternion.AngleAxis(rotY, -Vector3.right);
+
+                    transform.localRotation = ogRot * yQuat;
+                    break;
+                }
+        }
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if(angle < -360.0f)
+        {
+            angle += 360.0f;
+        }
+        else if(angle > 360.0f)
+        {
+            angle -= 360.0f;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ ProjectHorror/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float gravity;
+
+    private Vector3 moveDir = Vector3.zero;
+    private CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded())
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        cc.Move(moveDir * Time.deltaTime);
+    }
+
+    bool isGrounded()
+    {
+        if(cc.isGrounded)
+        {
+            return true;
+        }
+
+        Vector3 bottom = cc.transform.position - new Vector3(0, cc.height / 2, 0);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(bottom, -Vector3.up, out hit, 0.5f))
+        {
+            cc.Move(new Vector3(0, -hit.distance, 0));
+            return true;
+        }
+
+        return false;
+    }
+}
