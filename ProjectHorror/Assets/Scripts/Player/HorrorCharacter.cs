0f088e12df158c8537f63a18cbb593bb8ed12d97
John McCormack <silentsammy777@hotmail.com>
2019-07-05 00:24:31 +0100

    added interactable class and added bones of horror character class

--- ProjectHorror/Assets/Scripts/Player/HorrorCharacter.cs
+++ ProjectHorror/Assets/Scripts/Player/HorrorCharacter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HorrorCharacter : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,19 @@ public class HorrorCharacter : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Raycast();
+    }
+
+    void Raycast()
+    {
+        Vector3 rayDirection  = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        Ray ray = new Ray(rayDirection, Camera.main.transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * 2.0f, Color.red);
     }
 }
