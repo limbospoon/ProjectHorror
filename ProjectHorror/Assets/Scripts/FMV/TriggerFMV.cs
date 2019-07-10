b62c332024fd99534ff00f705aa89a3ead310235
John McCormack <silentsammy777@hotmail.com>
2019-07-10 21:50:03 +0100

    vid playback working

--- ProjectHorror/Assets/Scripts/FMV/TriggerFMV.cs
+++ ProjectHorror/Assets/Scripts/FMV/TriggerFMV.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class TriggerFMV : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public VideoPlayer videoPlayer;
+    public GameObject vidCam;
+
+    private GameObject playerRef;
+
+    private void Start()
     {
-        
+        if(!GetComponent<VideoPlayer>())
+        {
+            Debug.LogError("No video player found!!!");
+            return;
+        }
+
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.loopPointReached += OnMovieFinished;
+        videoPlayer.prepareCompleted += OnMoviePrepared;
+
+        vidCam.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMovieFinished(VideoPlayer vp)
     {
-        
+        playerRef.SetActive(true);
+        vidCam.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+
+    void OnMoviePrepared(VideoPlayer vp)
+    {
+        videoPlayer.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO: Make sure its the player
+        playerRef = other.gameObject;
+        other.gameObject.SetActive(false);
+        vidCam.SetActive(true);
+        videoPlayer.Prepare();
     }
 }
