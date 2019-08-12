b96d26612c137d2b3a0f3673824289310c81f0ef
John McCormack <silentsammy777@hotmail.com>
2019-08-12 17:40:51 +0100

    added inventory window class

--- ProjectHorror/Assets/Scripts/UI/UIHandler.cs
+++ ProjectHorror/Assets/Scripts/UI/UIHandler.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class UIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum CurrentWindow
     {
-        
+        None,
+        Inventory
+    }
+    public CurrentWindow currentWindow;
+
+    private void Awake()
+    {
+        currentWindow = CurrentWindow.None;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+               
+    }
+
+    private void OpenWindow(string windowName)
+    {
+        switch(windowName)
+        {
+            case "None":
+                {
+                    currentWindow = CurrentWindow.None;
+                    break;
+                }
+            case "Inventory":
+                {
+                    currentWindow = CurrentWindow.Inventory;
+                    break;
+                }
+        }
+
     }
 }
